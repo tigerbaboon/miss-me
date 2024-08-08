49db1d36503d89c2d344d5f251cea3010737f6aa
Tigerbaboon <45224355+tigerbaboon@users.noreply.github.com>
2024-08-08 17:56:06 +0700

    [ + ] update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: "2024-07-03",
   devtools: { enabled: false },
   modules: ["@nuxtjs/tailwindcss"],
   css: ["~/assets/css/main.css"],
